8c8ac83e786e9469af43b15bdb9bad3fcdf0917f
vinothr196 <vinothr196@gmail.com>
2020-02-15 12:25:18 +0530

    assessment/vinoth_r : Established One-to-Many relationship between Vehicle Make and Model

--- app/Models/VehicleMakes.php
+++ app/Models/VehicleMakes.php
@@ -6,4 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class VehicleMakes extends Model {
   protected $guarded = ['id'];
+
+  /**
+    * Get all vehicle-models of a make
+    * one-to-many relationship
+    */
+  public function vehicle_models()
+  {
+  	return $this->hasMany('App\Models\VehicleModels', 'vehicle_make_id');
+  }
 }
